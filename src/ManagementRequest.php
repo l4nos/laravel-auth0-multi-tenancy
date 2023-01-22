e0fcf6bb325161bb6c838df0159389d1f86cb9cf
Robert Lane <rob@nulead.co.uk>
2023-01-22 00:58:05 +0000

    Initial Commit - WIP

--- src/ManagementRequest.php
+++ src/ManagementRequest.php
@@ -3,6 +3,7 @@
 namespace Lanos\Auth0MultiManagement;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\RequestOptions;
 use Illuminate\Support\Facades\Log;
 
@@ -15,6 +16,7 @@ class ManagementRequest
     private $base_url;
     private $token_url;
     private $api_version;
+    private $access_token;
 
     public function __construct(){
         $this->client_id = config('authz.credentials.client_Id');
@@ -25,7 +27,29 @@ class ManagementRequest
         $this->audience = config('authz.audience');
     }
 
+    /**
+     * First checks for the access token, retrieves it if needed...
+     * Then constructs the HTTP request using Guzzle
+     * @param $method
+     * @param $path
+     * @param $query_params
+     * @param $json
+     * @return \Exception|mixed
+     * @throws GuzzleException
+     */
     public function request($method, $path, $query_params = null, $json = null){
+
+        // CHECK TOKEN FIRST
+
+        if(!isset($this->access_token)){
+            $tokenRequest = $this->retrieveToken();
+            if(isset($tokenRequest->access_token)){
+                $this->access_token = $tokenRequest->access_token;
+            }else{
+                Log::error('Unable to retrieve access token from Auth0');
+                throw new \Exception('Unable to retrieve access token from Auth0', 500);
+            }
+        }
 
         $url = $this->base_url . $path;
         $headers = [
@@ -33,8 +57,32 @@ class ManagementRequest
             'Content-Type' => 'application/json',
             'Authorization' => 'Bearer ' . $this->access_token,
         ];
+
+        $options = [
+            RequestOptions::QUERY => $query_params,
+            RequestOptions::HEADERS => $headers,
+            RequestOptions::JSON => $json
+        ];
+
+        $client = new Client($options);
+
+        try{
+
+            $response = $client->request($method, $url);
+            return json_decode($response->getBody()->getContents());
+
+        }catch (\Exception $exception){
+            Log::error($exception->getMessage());
+            return $exception;
+        }
+
     }
 
+    /**
+     * Function for retrieving management access token from Auth0
+     * @return \Exception|mixed
+     * @throws GuzzleException
+     */
     public function retrieveToken(){
 
         $headers = [
@@ -56,7 +104,7 @@ class ManagementRequest
         try{
             $response = $client->request('POST', $this->token_url);
             return json_decode($response->getBody()->getContents());
-        }catch (\Exception $exception){
+        }catch (GuzzleException $exception){
             Log::error($exception->getMessage());
             return $exception;
         }
